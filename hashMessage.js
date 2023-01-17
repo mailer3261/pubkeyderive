1b012749925425b7f692a08ee70886a5c7c2d932
gaccheshubham <mailer3261@gmail.com>
2023-01-17 10:57:21 +0530

    initial

---
+++ hashMessage.js
@@ -0,0 +1,9 @@
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    const hash = keccak256(utf8ToBytes(message))
+    return((hash))
+}
+
+module.exports = hashMessage;
